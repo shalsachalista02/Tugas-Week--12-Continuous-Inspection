c5bf1d785620549180e1f74f50a79e8faaad7b7b
shalsachalista02 <137140181+shalsachalista02@users.noreply.github.com>
2025-05-23 19:51:37 +0700

    commit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,11 +8,15 @@ public class Driver {
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.increment();
+		counter.increaseBy(5);
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.decrement();
+		counter.decreaseBy(2);
+		
+		System.out.println("Current count:" + counter.getCount());
+
+		counter.multiplyBy(6);
 		
 		System.out.println("Current count:" + counter.getCount());
 		
